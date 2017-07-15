62d616fa276eac6610adfa7c0a34a91fd921ab20
lewis <847055719@qq.com>
2017-07-15 23:04:13 +0800

    six

--- Pd.Api/Pd.Api/Areas/Admin/Controllers/UserController.cs
+++ Pd.Api/Pd.Api/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Models.SysModels;
+﻿using IServices.ISysServices;
+using Models.SysModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,52 @@ namespace Pd.Api.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ISysUserService _isysUserService;//用户表
+
+        public UserController(ISysUserService isysUserService)
+        {
+            _isysUserService = isysUserService;
+        }
+
         // GET: Admin/User
         public ActionResult Index()
         {
-            var list = new List<SysUser>();
+            ViewBag.tag = "用户管理";
 
+             var list=_isysUserService.GetAll().Select(t=>new{
+                  t.UserName,
+                  t.Remark,
+                  t.Id
+              });
+
+            
             return View(list);
         }
+
+        public ActionResult Edit(Guid? id)
+        {
+            var user = new SysUser();
+            ViewBag.tag = "用户新增";
+            if (id.HasValue)
+            {
+                user = _isysUserService.GetById(id.Value);
+                ViewBag.tag = "用户编辑";
+            }
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(Guid? id,SysUser Collection)
+        {
+            if (!ModelState.IsValid)
+            {
+                Edit(id);
+                return View(Collection);
+            }
+            _isysUserService.Save(id,Collection);
+            _isysUserService.Commit();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
